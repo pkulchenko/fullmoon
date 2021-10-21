da69f17635836fc8c19eeb899ef2bf6d2490889c
Paul Kulchenko <paul@kulchenko.com>
2021-10-20 19:05:06 -0700

    Added initial route processing.

--- fullmoon.lua
+++ fullmoon.lua
@@ -1,5 +1,5 @@
 --
--- nano-webframework implementation for Redbean web server (https://redbean.dev/)
+-- ultra-light webframework for Redbean web server (https://redbean.dev/)
 -- Copyright 2021 Paul Kulchenko
 -- 
 
@@ -7,6 +7,7 @@
 
 local Write = Write or io.write
 local EscapeHtml = EscapeHtml or function(s) return (string.gsub(s, "&", "&amp;"):gsub('"', "&quot;"):gsub("<","&lt;"):gsub(">","&gt;")) end
+local re = re or {compile = function() return {search = function() return end} end}
 
 if not setfenv then -- Lua 5.2+; this assumes f is a function
   -- based on http://lua-users.org/lists/lua-l/2010-06/msg00314.html
@@ -52,8 +53,6 @@ local ref = "params"
 local function render(name, opt)
   argerror(type(name) == "string", "bad argument #1 to render (string expected)")
   argerror(templates[name], "bad argument #1 to render (unknown template name)")
-  -- take parameters from the caller (if any); useful for `include` in templates
-  local parent = debug.getinfo(2, "f").func
   -- add local variables from the current environment
   local params = addlocals(getfenv(templates[name])[ref] or {})
   -- add explicitly passed parameters
@@ -85,99 +84,166 @@ local function addtemplate(name, code, opt)
   templates[name] = setfenv(assert((loadstring or load)(parse(code), code)), env)
 end
 
---[[-- return library --]]--
+--[[-- routing engine --]]--
 
-if pcall(debug.getlocal, 4, 1) then return {addtemplate = addtemplate, render = render} end
-
---[[-- run tests if launched as a script --]]--
-
-local out = ""
-Write = function(s) out = out..s end
-local num = 1
-local section = ""
-local function outformat(s) return type(s) == "string" and ("%q"):format(s):gsub("\n","n") or tostring(s) end
-local function is(result, expected, message)
-  local ok = result == expected
-  local msg = ("%s %d\t%s%s"):format((ok and "ok" or "not ok"), num, (section > "" and section.." " or ""), message)
-  if not ok then
-    msg = msg .. ("\n\treceived: %s\n\texpected: %s"):format(outformat(result), outformat(expected))
+local routes = {}
+local setmap = {["%d"] = "0-9", ["%w"] = "a-zA-Z0-9", ["\\d"] = "0-9", ["\\w"] = "a-zA-Z0-9"}
+local function route2regex(route)
+  -- foo/bar, foo/*, foo/:bar, foo/:bar[%d], foo(/:bar(/:more))(.:ext)
+  local params = {}
+  local regex, subnum = string.gsub(route, "%)", "%1?") -- update optional groups from () to ()?
+  :gsub("%.", "\\.") -- escape dots (.)
+  :gsub(":(%w+)", function(param) table.insert(params, param); return "([^/]+)" end)
+  :gsub("(%b[])(%+%))(%b[])", "%3%2") -- handle custom sets
+  :gsub("%b[]", function(s) return s:gsub("[%%\\][wd]", setmap) end)
+  :gsub("%*", "(.*)") -- add splat
+  argerror(subnum <= 1, "bad argument #1: more than one splat ('*') found", 4)
+  if subnum > 0 then table.insert(params, "splat") end
+  -- mark optional captures, as they are going to be returned during match
+  subnum = 1
+  local s, e, capture = 0
+  while true do
+    s, e, capture = regex:find("%b()([?]?)", s+1)
+    if not s then break end
+    if capture > "" then table.insert(params, subnum, false) end
+    subnum = subnum + 1
   end
-  print(msg)
-  num = num + 1
-  out = ""
+  return "^"..regex.."$", params
 end
+
+local function addroute(route, handler, opt)
+  local pos = routes[route] or #routes+1
+  local regex, params = route2regex(route)
+  routes[pos] = {route = route, handler = handler, options = opt, regex = regex, comp = re.compile(regex), params = params}
+  routes[route] = pos
+end
+
+local function match(path)
+  for _, route in ipairs(routes) do
+    -- skip static routes that are only used for path generation
+    if type(route.handler) == "function" then
+      local res = {route.comp:search(path)}
+      if table.remove(res, 1) then -- path matched
+        local params = {}
+        for ind, val in ipairs(route.params) do
+          if val then params[val] = res[ind] end
+        end
+        local res = route.handler({params = params})
+        if res then return res end
+      end
+    end
+  end
+end
+
+--[[-- various tests --]]--
+
+local tests = function()
+  local out = ""
+  Write = function(s) out = out..s end
+  local num = 1
+  local section = ""
+  local function outformat(s) return type(s) == "string" and ("%q"):format(s):gsub("\n","n") or tostring(s) end
+  local function is(result, expected, message)
+    local ok = result == expected
+    local msg = ("%s %d\t%s%s"):format((ok and "ok" or "not ok"), num, (section > "" and section.." " or ""), message)
+    if not ok then
+      msg = msg .. ("\n\treceived: %s\n\texpected: %s"):format(outformat(result), outformat(expected))
+    end
+    print(msg)
+    num = num + 1
+    out = ""
+  end
 
 --[[-- template engine tests --]]--
 
-section = "(template)"
-local tmpl1 = "tmpl1"
-addtemplate(tmpl1, "Hello, World!")
-render(tmpl1)
-is(out, "Hello, World!", "text rendering")
+  section = "(template)"
+  local tmpl1 = "tmpl1"
+  addtemplate(tmpl1, "Hello, World!")
+  render(tmpl1)
+  is(out, "Hello, World!", "text rendering")
 
-addtemplate(tmpl1, "Hello, {%& title %}!")
-render(tmpl1, {title = "World"})
-is(out, "Hello, World!", "text with parameter")
+  addtemplate(tmpl1, "Hello, {%& title %}!")
+  render(tmpl1, {title = "World"})
+  is(out, "Hello, World!", "text with parameter")
 
-render(tmpl1, {title = "World&"})
-is(out, "Hello, World&amp;!", "text with encoded parameter")
+  render(tmpl1, {title = "World&"})
+  is(out, "Hello, World&amp;!", "text with encoded parameter")
 
-addtemplate(tmpl1, "Hello, {% for i, v in ipairs({3,2,1}) do %}-{%= v %}{% end %}")
-render(tmpl1)
-is(out, "Hello, -3-2-1", "Lua code")
+  addtemplate(tmpl1, "Hello, {% for i, v in ipairs({3,2,1}) do %}-{%= v %}{% end %}")
+  render(tmpl1)
+  is(out, "Hello, -3-2-1", "Lua code")
 
-local tmpl2 = "tmpl2"
-addtemplate(tmpl2, [[{a: "{%= title %}"}]])
-render(tmpl2)
-is(out, '{a: ""}', "JSON with empty local value")
-
-do
-  addtemplate(tmpl2, [[{a: "{%= title %}"}]], {title = "set when adding template"})
+  local tmpl2 = "tmpl2"
+  addtemplate(tmpl2, [[{a: "{%= title %}"}]])
   render(tmpl2)
-  is(out, '{a: "set when adding template"}', "JSON with value set when adding template")
-  
-  local title = "local value"
-  render(tmpl2)
-  is(out, '{a: "local value"}', "JSON with local value")
+  is(out, '{a: ""}', "JSON with empty local value")
 
-  render(tmpl2, {title = "set from render"})
-  is(out, '{a: "set from render"}', "JSON with a passed value set at rendering")
+  do
+    addtemplate(tmpl2, [[{a: "{%= title %}"}]], {title = "set when adding template"})
+    render(tmpl2)
+    is(out, '{a: "set when adding template"}', "JSON with value set when adding template")
 
-  addtemplate(tmpl2, [[{% local title = "set from template" %}{a: "{%= title %}"}]])
-  render(tmpl2)
-  is(out, '{a: "set from template"}', "JSON with value set from template")
+    local title = "local value" -- to provide a value for the template
+    render(tmpl2)
+    is(out, '{a: "local value"}', "JSON with local value")
 
-  addtemplate(tmpl2, [[{a: "{%= title %}"}]], {title = "set when adding"})
+    render(tmpl2, {title = "set from render"})
+    is(out, '{a: "set from render"}', "JSON with a passed value set at rendering")
+
+    addtemplate(tmpl2, [[{% local title = "set from template" %}{a: "{%= title %}"}]])
+    render(tmpl2)
+    is(out, '{a: "set from template"}', "JSON with value set from template")
+
+    addtemplate(tmpl2, [[{a: "{%= title %}"}]], {title = "set when adding"})
+    render(tmpl2)
+    is(out, '{a: "local value"}', "JSON with local value overwriting the one set when adding template")
+  end
+
+  addtemplate(tmpl1, "Hello, {% include('tmpl2') %}")
+  render(tmpl1)
+  is(out, [[Hello, {a: "local value"}]], "`include` other template with a local value")
+
+  addtemplate(tmpl1, [[Hello, {% include('tmpl2', {title = "value"}) %}]])
+  render(tmpl1)
+  is(out, [[Hello, {a: "value"}]], "`include` other template with passed value set at rendering")
+
+  addtemplate(tmpl1, [[Hello, {% local title = "another value"; include('tmpl2') %}]])
+  render(tmpl1)
+  is(out, [[Hello, {a: "another value"}]], "`include` other template with value set from template")
+
+  addtemplate(tmpl1, "Hello, World!\n{% main() %}")
+  local _, err = pcall(render, tmpl1)
+  is(err ~= nil, true, "report Lua error in template")
+  is(err:match('string "Hello, World!'), 'string "Hello, World!', "error references original template code")
+  is(err:match(':2: '), ':2: ', "error references expected line number")
+
+  addtemplate(tmpl1, "Hello, {% main() %}World!", {main = function() end})
+  render(tmpl1)
+  is(out, [[Hello, World!]], "used function can be passed when adding template")
+
+  addtemplate(tmpl2, [[{% local function main() %}<h1>Title</h1>{% end %}{% include "tmpl1" %}]])
   render(tmpl2)
-  is(out, '{a: "local value"}', "JSON with local value overwriting the one set when adding template")
+  is(out, [[Hello, <h1>Title</h1>World!]], "function can be overwritten with template fragments in extended template")
+
+  addtemplate(tmpl2, [[{% local function main() Write"<h1>Title</h1>" end %}{% include "tmpl1" %}]])
+  render(tmpl2)
+  is(out, [[Hello, <h1>Title</h1>World!]], "function can be overwritten with direct write in extended template")
 end
 
-addtemplate(tmpl1, "Hello, {% include('tmpl2') %}")
-render(tmpl1)
-is(out, [[Hello, {a: "local value"}]], "`include` other template with a local value")
+--[[-- core engine --]]--
 
-addtemplate(tmpl1, [[Hello, {% include('tmpl2', {title = "value"}) %}]])
-render(tmpl1)
-is(out, [[Hello, {a: "value"}]], "`include` other template with passed value set at rendering")
+local function run(opt)
+  if opt.tests then tests(); os.exit() end
+  OnHttpRequest = function()
+    local res = match(GetPath():sub(1))
+    if not res then SetStatus(404, "Not Found") end
+  end
+end
 
-addtemplate(tmpl1, [[Hello, {% local title = "another value"; include('tmpl2') %}]])
-render(tmpl1)
-is(out, [[Hello, {a: "another value"}]], "`include` other template with value set from template")
+-- return library if called with `require`
+if pcall(debug.getlocal, 4, 1) then
+  return {addtemplate = addtemplate, render = render, addroute = addroute, run = run}
+end
 
-addtemplate(tmpl1, "Hello, World!\n{% main() %}")
-local ok, err = pcall(render, tmpl1)
-is(err ~= nil, true, "report Lua error in template")
-is(err:match('string "Hello, World!'), 'string "Hello, World!', "error references original template code")
-is(err:match(':2: '), ':2: ', "error references expected line number")
-
-addtemplate(tmpl1, "Hello, {% main() %}World!", {main = function() end})
-render(tmpl1)
-is(out, [[Hello, World!]], "used function can be passed when adding template")
-
-addtemplate(tmpl2, [[{% local function main() %}<h1>Title</h1>{% end %}{% include "tmpl1" %}]])
-render(tmpl2)
-is(out, [[Hello, <h1>Title</h1>World!]], "function can be overwritten with template fragments in extended template")
-
-addtemplate(tmpl2, [[{% local function main() Write"<h1>Title</h1>" end %}{% include "tmpl1" %}]])
-render(tmpl2)
-is(out, [[Hello, <h1>Title</h1>World!]], "function can be overwritten with direct write in extended template")
+-- run tests if launched as a script
+run{tests = true}
